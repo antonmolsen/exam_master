cb51721d66243c877964514ed850be5d5babadf6
antonmolsen <81230944+antonmolsen@users.noreply.github.com>
2021-06-14 09:55:10 +0200

    new file

---
+++ functions/displayMenu.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def inputNumber(prompt):
+    while True:
+        try:
+            num = float(input(prompt))
+            break
+        except ValueError:
+            pass
+
+    return num
+
+
+def displayMenu(options):
+
+    for i in range(len(options)):
+        print("{:d}. {:s}".format(i+1, options[i]))
+
+    choice = 0
+    while True:
+        try:
+            choice = inputNumber("Please choose a menu item: ")
+            if(choice == 0):
+                break
+            elif(not(np.any(choice == np.arange(len(options))+1))):
+                raise
+            break
+
+        except:
+            print('Please those a number from the list')
+
+    return choice
