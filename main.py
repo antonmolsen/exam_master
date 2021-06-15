8a9a4553fadc7842459142dedb1c8863acb86909
NicolaiCK <nicolai.kongstad@gmail.com>
2021-06-15 09:29:35 +0200

    Libraries

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from functions.beamDeflection import beamDeflection
 from functions.beamSuperposition import beamSuperposition
 
-pos = np.array([1,2,3,4,5,54,45,45,45])
+pos = np.array([1, 2, 3, 4, 5, 54, 45, 45, 45])
 print("deflec", beamDeflection(pos, 20, 13, 50, "both"))
 
-print("defelc_sup_pos =", beamSuperposition(pos, 20, 13, np.array([30,40,50,50]), "both"))
+print("defelc_sup_pos =", beamSuperposition(pos, 20, 13, np.array([30, 40, 50, 50]), "both"))
 """
 Created on Mon Jun  7 22:15:28 2021
 
