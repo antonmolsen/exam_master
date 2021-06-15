1b716c9ef998962a694d09332028dba5926d5754
NicolaiCK <nicolai.kongstad@gmail.com>
2021-06-15 09:23:19 +0200

    Superposition

--- functions/beamSuperposition.py
+++ functions/beamSuperposition.py
@@ -1,18 +1,14 @@
 import numpy as np
 from functions.beamDeflection import beamDeflection
 
-def beamSuperposition(positions, beamLength, loadPositions, loadForces, beamSupport):
 
-    #there are no forces applied when loadPositions or loadForces are equal to zero
-    if loadPositions or loadForces == 0:
-        deflection = np.zeros(np.size(positions))
-    else : # we calculate the superposition of deflections
-        deflec
+def beamSuperposition(positions, beamLength, loadPositions, loadForces, beamSupport):
+    deflection = np.zeros(np.size(positions))
+
+    # there are no forces applied when loadPositions or loadForces are equal to zero
+    if not(np.all(loadPositions) or np.all(loadForces) == 0):
         for i in range(np.size(loadForces)):
             # loadForce i corresponds to loadPosition i
-            deflection_row = beamDeflection(positions, beamLength, loadPositions[i], loadForces[i], beamSupport)
-            np.vstack()
-
-        deflection = deflection.sum(axis=0)
-
+            deflection += beamDeflection(positions, beamLength,
+                                         loadPositions[i], loadForces[i], beamSupport)
     return deflection
