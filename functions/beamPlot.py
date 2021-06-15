a95d7b8106e0cea5c943e922b994217fdde68768
NicolaiCK <nicolai.kongstad@gmail.com>
2021-06-15 11:19:37 +0200

    ARROWS

--- functions/beamPlot.py
+++ functions/beamPlot.py
@@ -4,15 +4,15 @@ from functions.beamSuperposition import *
 
 
 def beamPlot(beamLength, loadPositions, loadForces, beamSupport):
-    positions = np.arange(0, beamLength, 200)
+    positions = np.arange(0, beamLength, beamLength/2000)
     height = beamSuperposition(positions, beamLength, loadPositions, loadForces, beamSupport)
-
-    plt.plot(positions, height, 'or--')
+    
+    plt.plot(positions, height, 'r-')
     for i in range(np.size(loadPositions)):
         loadHeight = beamSuperposition(
             np.array([loadPositions[i]]), beamLength, loadPositions, loadForces, beamSupport)
-        plt.arrow(loadPositions[i], loadHeight[0]*(15/10), 0, loadHeight[0]**(11/10))
-        plt.text(loadPositions[i], loadHeight[0]*2, r'$W_{{{}}}$'.format(i + 1))
+        plt.arrow(loadPositions[i], loadHeight[0]*0.85, 0, loadHeight[0]*0.05, head_width=beamLength*0.02, head_length=abs(loadHeight[0]*0.05))
+        plt.text(loadPositions[i]*(95/100), loadHeight[0]*0.8, r'$W_{{{}}}$'.format(i + 1))
 
-    plt.xlim([-1, beamLength + 1])
+    plt.xlim([-beamLength*(1/10), beamLength*(11/10)])
     plt.show()
--- functions/beamSuperposition.py
+++ functions/beamSuperposition.py
@@ -6,8 +6,7 @@ def beamSuperposition(positions, beamLength, loadPositions, loadForces, beamSupp
     deflection = np.zeros(np.size(positions))
 
     # there are no forces applied when loadPositions or loadForces are equal to zero
-    if not(np.all(loadPositions) or np.all(loadForces) == 0):
-        for i in range(np.size(loadForces)):
+    for i in range(np.size(loadForces)):
             # loadForce i corresponds to loadPosition i
             deflection -= beamDeflection(positions, beamLength, loadPositions[i], loadForces[i], beamSupport)
     return deflection
