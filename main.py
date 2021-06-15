e13ea732964a01802a4b35755cb84b65b443e34e
antonmolsen <81230944+antonmolsen@users.noreply.github.com>
2021-06-15 12:14:24 +0200

    kæmpe main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 
 from functions.displayMenu import *
 from functions.beamDeflection import beamDeflection
@@ -8,17 +9,14 @@ from functions.beamPlot import beamPlot
 
 # Main script
 
-# Initial conditions
-loadForces = []
-loadPositions = []
-positions = []
+# can only run one beam at a time
 
-#empty dataframe as inital
-df = {"lengthBeam":  [1,2,3],
-      "supportBeam": [1,2,3],
-      "loadPosition": [1,2,3],
-      "forcePosition": [1,2,3]}
-df = pd.DataFrame(df, columns = ["lengthBeam", "supportBeam", "loadPosition", "forcePosition"])
+# Initial conditions
+#empty dataframe as initial
+df = pd.DataFrame({"lengthBeam":  [0],
+      "supportBeam": [0],
+      "loadPosition": [],
+      "forcePosition": []})
 
 while True:
     menuItems = np.array(["Configure beam", "Configure loads", "Save beam and loads", "Load beam and loads", "Generate plot", "Quit"])
@@ -33,9 +31,9 @@ while True:
         elif supportChoice == 2:
             support = "cantilever"
 
-        # data is appended to length and support list
-
-
+        df.iloc[0,0] = length_current_beam
+        df.iloc[1,0] = support
+        #first row of dataframe is reserved for beam information
 
     if mainChoice == 2: # Configure loads
         while True:
@@ -44,14 +42,13 @@ while True:
             loadChoice = displayMenu(loadItems)
 
             if loadChoice == 1: # See current loads
-                if np.sum(loadForces) == 0:
-                    print("there are currently no load forces on the beam")
+                if sum(df.loadPosition) or sum(df.forcePosition) == 0:
+                    print("There are currently no load forces on the beam")
                     pass
-
                 else:
-                    print("current loads are \n")
-                    for i in range(np.size(loadForces))
-                        print("force at {} meters with {} newton".format(loadPositions[i], loadForces[i]))
+                    print("current loads and forces are \n")
+
+                    # PRINT DATFRAME HERE
 
             if loadChoice == 2: # Add a load
                 while True:
@@ -59,21 +56,60 @@ while True:
                         load_pos = inputNumber("Enter position of load in meters: ")
                         force_val = inputNumber("Enter the force at the position: ")
 
-                        if load_pos < 0:
+                        if load_pos < 0 or load_pos > #beamkength :
+                            raise
+
+                        # load position and load force is appended to df
+                        df = df.append({"loadPosition": load_pos, "forcePosition": force_val}, ignore_index=True)
+                        break
+                    except:
+                        print("Your load position can not be lower than 0. ")
+            if loadChoice == 3: # Remove a load
+                #prints dataframe and the user can select which line to remove
+
+
+
+
+                while True:
+                    try:
+
+                        force_val = inputNumber("Enter the force at the position: ")
+
+                        if len(loadPosition) == 0:
                             raise
                         break
                     except:
-                        print("your load position can not be lower than 0. ")
-            if loadChoice == 3: # Remove a load
+                        print("There are currently no load forces on the beam")
+
+
+
 
 
     if mainChoice == 3: # Save beam and loads
-        "cantilever"
+        saving_filename = input("What do you wish to name your file ?: ")
+
+        s = df.to_csv(index=False)
+        f = open("beam_and_support_data.csv", "w") #write
+        f.write(s)
+        f.close()
+        #print("file saved as {} in {}".format(saving_filename, cd))
+
+
+
     if mainChoice == 4: # Load beam and loads
+        load_filename = input("Please enter the csv file you wish to load")
+
+        df = pd.read_csv(load_filename)
+
+        #use dataload funktion
+
+        print("Data loaded")
 
         "cantilever"
     if mainChoice == 5: # Generate plot
         # beamPlot(beamLength, ) runs here
+        #ekstra plot function ?
+        #print list of current forces in console
 
         print("Beam plot created succesfully")
 
