d3ef9e75880b906f981058b474c77775064dcfbd
NicolaiCK <nicolai.kongstad@gmail.com>
2021-06-16 12:26:20 +0200

    inovwev

--- main.py
+++ main.py
@@ -118,28 +118,28 @@ while True:
 
     if mainChoice == 3:  # Save beam and loads
         while True:
-            # try:
-            saving_filename = input("What do you wish to name your file ?: ")
-            df_for_saving = df
-            if os.path.isfile(saving_filename + ".csv"):
-                raise
-            df_for_saving.insert(2, "beamLength", np.nan, False)
-            df_for_saving.insert(3, "beamSupport", '', False)
-
-            df_for_saving.at[0, "beamLength"] = beamLength
-            df_for_saving.at[0, "beamSupport"] = beamSupport
-
-            # file shall not overwrite old file
-            s = df_for_saving.to_csv(index=False)
-            f = open(saving_filename + ".csv", "w")  # write
-            f.write(s)
-            f.close()
-            cwd = os.getcwd()
-            print('File saved as "{}" in "{}"'.format(saving_filename, cwd))
-
-            break
-            # except:
-            print("File already exists. Please enter another filename")
+            try:
+                saving_filename = input("What do you wish to name your file ?: ")
+                df_for_saving = df
+                if os.path.isfile(saving_filename + ".csv"):
+                    raise
+                df_for_saving.insert(2, "beamLength", np.nan, False)
+                df_for_saving.insert(3, "beamSupport", '', False)
+    
+                df_for_saving.at[0, "beamLength"] = beamLength
+                df_for_saving.at[0, "beamSupport"] = beamSupport
+    
+                # file shall not overwrite old file
+                s = df_for_saving.to_csv(index=False)
+                f = open(saving_filename + ".csv", "w")  # write
+                f.write(s)
+                f.close()
+                cwd = os.getcwd()
+                print('File saved as "{}" in "{}"'.format(saving_filename, cwd))
+    
+                break
+            except:
+                print("File already exists. Please enter another filename")
 
     if mainChoice == 4:  # Load beam and loads
         load_filename = input("Please enter the csv file you wish to load")
