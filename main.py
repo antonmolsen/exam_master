0236c7b64b359d9d762e4272d543a644baaae0cc
NicolaiCK <nicolai.kongstad@gmail.com>
2021-06-17 10:12:30 +0200

    153189+

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ while True:
 
                         # removal of forces from string input
                         removed_forces = inputString(
-                            'Please enter a list of the forces you wish to remove, e.g. "W1,W2" ', 'wW1234, ')
+                            'Please enter a list of the forces you wish to remove, e.g. "W1,W2" ', 'wW1234567890, ')
                         removed_forces = np.fromstring(
                             removed_forces.upper().replace("W", ""), dtype=int, sep=',')
 
